df8b8f9bb2ead5e9372dce01fd50989a6b35e601
Dulcelene Machado <dudutola@gmail.com>
2025-04-16 12:30:23 +0100

    Added recurrence logic for 'Every Monday' and switched task list ul to table layout

--- app/models/task.rb
+++ app/models/task.rb
@@ -9,6 +9,12 @@ class Task < ApplicationRecord
   def next_recurrence
     if recurrence == "Every Day"
       due_date + 1.day
+    elsif recurrence == "Every Monday"
+      today = Date.today
+      days_until_monday = (3 - today.wday) % 7
+      days_until_monday = 7 if days_until_monday == 0
+      next_monday = today + days_until_monday.days
+      next_monday
     else
       due_date
     end

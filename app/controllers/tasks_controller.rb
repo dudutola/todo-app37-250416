57be475f2fc865002dfe5218dd06e0ec8b8618c5
Dulcelene Machado <dudutola@gmail.com>
2025-04-16 10:57:55 +0100

    Added tasks#new to create new tasks Fixed form submission and error display for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,23 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "Task was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :due_date, :recurrence, :completed)
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ApplicationRecord
   validates :title, presence: true
   validates :due_date, presence: true
   validates :recurrence, inclusion: {
-    in: [ "Evey Day", "Every Monday" ],
+    in: [ "None", "Every Day", "Every Monday" ],
     message: "%{value} is not a valid recurrence option"
   }, allow_blank: true
 end

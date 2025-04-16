eb45cbf40ff58fee67c4b382410ad83f8fe85dfb
Dulcelene Machado <dudutola@gmail.com>
2025-04-16 09:49:28 +0100

    Added validations to task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,8 @@
 class Task < ApplicationRecord
+  validates :title, presence: true
+  validates :due_date, presence: true
+  validates :recurrence, inclusion: {
+    in: [ "Evey Day", "Every Monday" ],
+    message: "%{value} is not a valid recurrence option"
+  }, allow_blank: true
 end

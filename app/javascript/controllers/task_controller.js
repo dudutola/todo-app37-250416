70b449e78058774bb169d3c58faf345afe043890
Dulcelene Machado <dudutola@gmail.com>
2025-04-18 16:10:51 +0100

    Adjusted the datetime input logic to use local time instead of UTC in stimulus

--- app/javascript/controllers/task_controller.js
+++ app/javascript/controllers/task_controller.js
@@ -5,20 +5,28 @@ export default class extends Controller {
   static targets = [ "dueDate", "currentDueDate"]
 
   new(e) {
-    if (e.target.closest("form") || e.target.tagName === "INPUT") {
-      return
-    }
+    if (e.target.closest("form") || e.target.tagName === "INPUT") return;
 
-    e.preventDefault()
+    e.preventDefault();
 
     const date = e.currentTarget.dataset.taskDate
 
     const dialog = document.getElementById("new-task-dialog");
-    dialog.showModal()
+    dialog.showModal();
 
-    const currentDate = new Date(date)
-    const currentDateFormatted = currentDate.toISOString().slice(0, 16)
-    this.currentDueDateTarget.value = currentDateFormatted
+    const currentDate = new Date(date);
+    const now = new Date();
+    currentDate.setHours(now.getHours(), now.getMinutes())
+
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+    const day = String(currentDate.getDate()).padStart(2, "0");
+    const hours = String(currentDate.getHours()).padStart(2, "0");
+    const minutes = String(currentDate.getMinutes()).padStart(2, "0");
+
+    const currentDateFormatted = `${year}-${month}-${day}T${hours}:${minutes}`;
+    // const currentDateFormatted = currentDate.toISOString().slice(0, 16);
+    this.currentDueDateTarget.value = currentDateFormatted;
   }
 
   close(e) {
